01da74679a010a1687f908578475d62ebfeafbc3
dhani <rahmad@mamiteam.com>
2021-08-24 22:51:23 +0800

    render list from state and add basic styling for list

--- src/Components/List/TodoList.js
+++ src/Components/List/TodoList.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './TodoList.scss'
 
 const TodoList = () => {
-  return (
-    <div>This is our list</div>
-  );
+  const [todo] = useState([
+    {
+      name: "morning workout",
+      isDone: false
+    },
+    {
+      name: "wash the car",
+      isDone: true
+    }
+  ])
+  const mapTodo = todo.map((item) => {
+    return (
+      <div className="list">
+        <div>{item.name}</div>
+        <div>{item.isDone === false ? "Not Done" : "Done" }</div>
+      </div>
+    );
+  })
+  return mapTodo;
 }
 
 export default TodoList;
