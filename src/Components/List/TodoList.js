2b15d85cd494639baa289db1420e86f52aa3b7e2
dhani <rahmad@mamiteam.com>
2021-08-25 14:42:36 +0800

    map data on todo list using state data

--- src/Components/List/TodoList.js
+++ src/Components/List/TodoList.js
@@ -1,26 +1,61 @@
 import React, { useState } from 'react';
 import './TodoList.scss'
 
-const TodoList = () => {
-  const [todo] = useState([
+function TodoList() {
+  const [todo, setTodo] = useState([
     {
       name: "morning workout",
       isDone: false
     },
     {
       name: "wash the car",
-      isDone: true
+      isDone: false
+    },
+    {
+      name: "lunch",
+      isDone: false
     }
-  ])
-  const mapTodo = todo.map((item) => {
+  ]);
+
+  const onClickDone = index => e => {
+    let newArr = [...todo];
+    newArr[index].isDone = true;
+
+    setTodo(newArr);
+  }
+
+  const mapTodo = todo.map((item, index) => {
     return (
-      <div className="list">
+      <div className="list" key={index}>
         <div>{item.name}</div>
-        <div>{item.isDone === false ? "Not Done" : "Done" }</div>
+        <div>{item.isDone === false ? "Not Done" : "Done"}</div>
+        {item.isDone === false ? <div onClick={onClickDone(index)}>set as done</div> : "" }
       </div>
     );
   })
+
   return mapTodo;
 }
+
+// function TodoList() {
+//   const [count, setCount] = React.useState(0)
+
+//   React.useEffect(() => {
+//     const parsedCount = Number(localStorage.getItem("count") || 0)
+//     setCount(parsedCount)
+//   }, [])
+
+//   React.useEffect(() => {
+//     localStorage.setItem("count", count)
+//   }, [count])
+
+//   return (
+//     <div>
+//       <div>{count}</div>
+//       <button onClick={() => setCount(c => c + 1)}>+</button>
+//       <button onClick={() => setCount(c => c - 1)}>-</button>
+//     </div>
+//   )
+// }
 
 export default TodoList;
